5b69e1b4bd581048cf2daa6c2552892a109f88ae
Bill Thaman <william.thaman@gmail.com>
2022-02-10 17:14:57 -0600

    removed

--- Search.bat
+++
@@ -1,3 +0,0 @@
-REM use pythonw.exe to avoid a command window staying open during execution
-REM using start /min c:\... is not required
-start C:\Users\wjt\AppData\Local\Continuum\Anaconda3\envs\python3.4\pythonw.exe "C:/Users/wjt/Google Drive/Programming/Python/PycharmProjects/file_search/file_search_app.py"
